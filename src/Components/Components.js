d3252584ba8882bedd38dc49be00423bdaebcdeb
TimrayZ <51561712+TimrayZ@users.noreply.github.com>
2021-07-14 17:32:05 +0800

    Register Page

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,21 +1,33 @@
 import React from "react";
 import MainModule from "./Main/Main.js";
-import About from "./About/About";
+import About from "./SecondView/About";
 import Footer from "./Footer/Footer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
+import AuthModule from "./Auth/Auth.js";
+import AuthRegister from "./Auth/AuthRegister";
 
 const Components = () => {
   return (
     <div>
       <Router>
-      <div>
-        {/* <Home /> */}
-        <Route path="/main" exact component={MainModule} />
-        {/* <About /> */}
-        <Route path="/about" component={About} />
-      </div>
-      <Footer />
-    </Router>
+        <div>
+          {/* <Home /> */}
+          <Route path="/main" exact component={MainModule} />
+          {/* <About /> */}
+          <Route path="/about" component={About} />
+          <Route path="/auth" exact component={AuthModule} />
+          <Route path="/register" component={AuthRegister} />
+          {/* <Route path="/login" component={AuthLogin} />
+          <Route path="/main" component={MainModule} /> */}
+        </div>
+        <Redirect to="/auth" />
+        <Footer />
+      </Router>
     </div>
   );
 };
